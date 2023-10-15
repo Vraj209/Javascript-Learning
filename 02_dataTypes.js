f5f516c6992eb8f337ecaae52ab308ff97ed7c39
Vraj209 <80612219+Vraj209@users.noreply.github.com>
2023-10-14 21:48:01 -0400

    Datatypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer versions
+
+// alert(3 + 3); //we are using nodejs , not browser specific
+// code readablity should be high
+
+console.log(3 + 3);
+console.log("Vraj");
+
+let name = "vraj";
+let age = 23;
+let isLoggedIn = true;
+let state;
+
+// Primitive Datatype
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value => object
+// undefined => not assign value
+// symbol => unique
+
+// Non-Primitive Datatype
+// Object
+
+// console.log(typeof undefined) => undefined
+// console.log(typeof null) => object
