1277b33d8b291d0dd3653855da78c9d78aea6884
Vraj209 <80612219+Vraj209@users.noreply.github.com>
2023-10-14 22:17:09 -0400

    Added more conversion

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -24,3 +24,40 @@ let someNumber = 33;
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber); // string
+
+// ********** Operations *************
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 ** 3);
+console.log(2 / 3);
+console.log(2 % 3);
+
+let str1 = "hello";
+let str2 = " vraj";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); // All String 122
+console.log(1 + 2 + "2"); // 32
+
+// trick conversion - not usefull
+console.log(+true); // 1
+// console.log(true+); // err
+console.log(+""); // 0
+
+// Not readable
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++; //postfix
+++gameCounter; //prefix
+console.log(gameCounter);
